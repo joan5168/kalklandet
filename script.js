9325cd84677c2f3a76ab654c64a2cf2157947ad4
June150992 <44156812+June150992@users.noreply.github.com>
2019-04-10 09:29:50 +0200

    burgermenu

---
+++ script.js
@@ -0,0 +1,22 @@
+function myFunction() {
+            var x = document.getElementById("myLinks");
+            if (x.style.display === "block") {
+                x.style.display = "none";
+            } else {
+                x.style.display = "block";
+            }
+        }
+        window.onscroll = function() {
+            stickyFunction()
+        };
+
+        var header = document.getElementById("myBurgermenu");
+        var sticky = header.offsetTop;
+
+        function stickyFunction() {
+            if (window.pageYOffset > sticky) {
+                header.classList.add("sticky");
+            } else {
+                header.classList.remove("sticky");
+            }
+        }
