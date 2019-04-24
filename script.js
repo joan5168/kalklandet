84d98a227646904d45398ad48e3aa38fb9288329
Julie Hansen <juliehansen@overmind-the3rd.kea.local>
2019-04-24 11:47:47 +0200

    burgermenu er fixet

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 function myFunction() {
-            var x = document.getElementById("myLinks");
-            if (x.style.display === "block") {
-                x.style.display = "none";
-            } else {
-                x.style.display = "block";
-            }
-        }
-        window.onscroll = function() {
-            stickyFunction()
-        };
-
-        var header = document.getElementById("myBurgermenu");
-        var sticky = header.offsetTop;
-
-        function stickyFunction() {
-            if (window.pageYOffset > sticky) {
-                header.classList.add("sticky");
-            } else {
-                header.classList.remove("sticky");
-            }
-        }
-function closeFunction(x) {
-  x.classList.toggle("change");
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
 }
 
+window.onscroll = function () {
+    stickyFunction()
+};
+
+var header = document.getElementById("myBurgermenu");
+var sticky = header.offsetTop;
+
+function stickyFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
+
+function closeFunction(x) {
+    x.classList.toggle("change");
+}
